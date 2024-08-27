95ca995023f482d77d3bbc3770043551085f5305
Delmy Bonilla <bonildel001@warren.k12.in.us>
2024-08-27 08:33:33 -0400

    fixed file

---
+++ main.py
@@ -0,0 +1,7 @@
+miles = 3
+minutes = 60
+hours = minutes / 60
+speed = miles / hours
+print("How many miles did you run?", miles)
+print("How many minutes did it take you?", minutes)
+print ("Speed in mph:", speed)
